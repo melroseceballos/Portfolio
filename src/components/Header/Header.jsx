da5c6a7eec593163a0eb44a0ae0aea7224b89efa
Melrose Ceballos <mcm44@hawaii.edu>
2023-04-15 23:31:34 -0700

    added about page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,43 +2,21 @@ import './styles.css'
 
 
 function Header() {
-  function myFunction(x) {
-    x.classList.toggle("change");
-    const navLinks = document.querySelector('.nav-links');
-    navLinks.classList.toggle('show');
+    return (
+        <>
+          <header>
+            <nav className='navigation'>
+            <li><a href="#">HOME</a></li>
+            <li><a href="#">ABOUT</a></li>
+            <li><a href="#">PROJECT</a></li>
+            <li><a href="#">CONTACT ME</a></li>
+          </nav>
+          </header>
+          
+        </>
+      )
   }
 
-  return (
-    <>
-      <header>
-        <nav className='navigation'>
-        <li><a href="#">HOME</a></li>
-        <li><a href="#">ABOUT</a></li>
-        <li><a href="#">PROJECT</a></li>
-        <li><a href="#">CONTACT ME</a></li>
-      </nav>
-      </header>
-      
-    </>
-  )
-}
-
+ 
+  
 export default Header
-
-{/* <nav>
-<div className='container'>
-            <div class="container" onClick={(event) => myFunction(event.target)}>
-              <div class="bar1"></div>
-              <div class="bar2"></div>
-              <div class="bar3"></div>
-            </div>
-            <div class="nav-links">
-              <ul>
-                <li><a href="#home">Home</a></li>
-                <li><a href="#about">About Me</a></li>
-                <li><a href="#projects">Projects</a></li>
-                <li><a href="#contact">Contact Me</a></li>
-              </ul>
-            </div>
-          </div>
-        </nav> */}
