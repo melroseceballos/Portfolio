3241d411981b700170f9d8efcf8417e425b52ec0
Melrose Ceballos <mcm44@hawaii.edu>
2023-04-17 23:11:23 -0700

    About test 2

--- src/components/AboutPage/about.jsx
+++ src/components/AboutPage/about.jsx
@@ -6,8 +6,20 @@ function About() {
     <div className="about-container">
       <img src="/AboutFirst.svg" alt='beesBG' className='aboutFirstSVG'/>
       <div className="about-text">
-        <h1 className="aboutIntro">Hi There! <br />I'm BUZZZED to meet you!</h1>
+        <h1 className="aboutIntro">Hi There, <br />My Name is Melrose Ceballos</h1>
+        <h3 className='buzz'>..and I'm so buzzed to meet you!</h3>
         <img className="pic" src="pfp.png"/>
+
+        <div className='paragraph'>
+          <p>I am a Full-Stack Software Engineer specializing in Front-End development and Databases. 
+            My favorite Front-end technologies are React.js and Vue.js, while my go-to backend technologies are Django and Express. 
+            When working on projects, I enjoy setting up models and routes, building REST APIs, and styling. 
+            I love being a Full-Stack software engineer because it challenges me to be efficient and innovative in my thinking,
+             while also allowing me to express my creativity in multiple ways!
+      </p>
+      </div>
+      
+
       </div>
     </div>
   );
