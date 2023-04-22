cdad263954510420ce82022421eed8acf289c055
Melrose Ceballos <mcm44@hawaii.edu>
2023-04-22 15:07:07 -0700

    fixed about css

--- src/components/AboutPage/about.jsx
+++ src/components/AboutPage/about.jsx
@@ -6,19 +6,8 @@ function About() {
       <div className="about-text">
       <h1 className="aboutIntro">Hi There <span className="hand-emoji" role="img" aria-label="Waving Hand">👋</span><br />My Name is Melrose Ceballos</h1>
         <h3 className='buzz'>..and I'm so buzzed to meet you!</h3>
-        <img className="pic" src="pfp.png"/>
-        <div>
-         <a href="#">
-         <span class="left"></span>
-         </a>
-         </div>
-         <div>
-         </div>
-         <div>
-         <a href="#">
-         <span class="right"></span>
-         </a>
         </div>
+        <div className="pic" ><img src="pfp.png"/></div>
         <div className='paragraph'>
           <p>I am a Full-Stack Software Engineer specializing in Front-End development and Databases. 
             My favorite Front-end technologies are React.js and Vue.js, while my go-to backend technologies are Django and Express. 
@@ -27,7 +16,7 @@ function About() {
              while also allowing me to express my creativity in multiple ways!
       </p>
       </div>
-      </div>
+
     </div>
   );
 }
